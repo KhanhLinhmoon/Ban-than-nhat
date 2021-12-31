cd60efc99595d3df0336578996f6345c06f7f556
unknown <hakhanhlinh536@gmail.com>
2021-12-31 23:50:48 +0700

    message

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Chin chaoo Ngát iu của tớ   ! 🖐🖐🖐',
+    introDesc: `Tớ có điều này muốn hỏi Ngát, Ngát nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Tớ biết Ngát thích tớ nhất phải không phải không ? 😙',
+    desc: 'Ngát không trả lời hoặc thoát ra, là bank tớ 500k nha :> ',
+    btnYes: 'Có, tớ thích Linh nhất 💖💖💖',
+    btnNo: '💔 Mơ à ! 💔 ',
+    question:'Nói cho tớ biết đi vì sao Ngát thích tớ 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì Linh xinh đẹp lại còn tốt bụng đó Ahiiiii 😊😊',
+    mess: 'Tớ biết mà, Tớ cũng thích Ngát nhìu lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin tớ nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/messages/t/100020906259462/'
+}
